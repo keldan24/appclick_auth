8e173102538118f82a8963eec0dddfa73a49fe47
Kelechi Daniel Onwumere <kelechidaniel171kd@gmail.com>
2023-12-14 04:55:31 +0100

    commited

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User 
-        fields = ['id', 'username', 'password', 'email', 'phone']
+        fields = ['id', 'first_name', 'last_name', 'username', 'password', 'email', 'phone']
--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     re_path('signup/', views.signup),
-    re_path('login', views.login),
-    re_path('test_token', views.test_token),
+    re_path('login/', views.login),
+    re_path('test_token/', views.test_token),
 ]
